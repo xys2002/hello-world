3812b402e08bd1dec0d710899a7461bb63481397
xys2002 <zcabxui@ucl.ac.uk>
2021-10-06 00:03:26 +0100

    q2 upload

--- q2.py
+++ q2.py
@@ -1,2 +1,13 @@
-def a():
-    print("a")
+#<Yunsheng Xu 2021-10-5>
+#This program is to tell the number which user input is odd or even
+def method(number):
+    num=int(number)
+    num_1=int(num/2)
+    print(num,num_1)    #check if it is going on well
+    if num!=num_1*2:
+        print("odd")
+    else:
+        print("even")
+Input=input("input:")
+method(Input)
+    
